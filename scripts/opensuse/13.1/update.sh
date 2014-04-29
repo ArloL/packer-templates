cc1d65f54745ebce8e9567d4994871ac074965bd
Christian Berendt <berendt@b1-systems.de>
2014-04-29 17:22:14 +0200

    added repsitory opensuse-13.1-update-non-oss

--- scripts/opensuse/13.1/update.sh
+++ scripts/opensuse/13.1/update.sh
@@ -6,5 +6,6 @@ rpm --import http://download.opensuse.org/distribution/13.1/repo/oss/gpg-pubkey-
 zypper ar http://download.opensuse.org/distribution/13.1/repo/oss/ opensuse-13.1-oss
 zypper ar http://download.opensuse.org/distribution/13.1/repo/non-oss/ opensuse-13.1-non-oss
 zypper ar http://download.opensuse.org/update/13.1/ opensuse-13.1-update
+zypper ar http://download.opensuse.org/update/13.1-non-oss/ opensuse-13.1-update-non-oss
 zypper refresh
 zypper update -y
