67d2b006535abab91496f34526175cd5ede61f34
Christian Berendt <berendt@b1-systems.de>
2014-06-04 18:56:06 +0200

    removed alpine 2.7.8, changed to v3

--- scripts/alpine/update.sh
+++ scripts/alpine/update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -eux
 
 cp /etc/apk/repositories /etc/apk/repositories.orig
-echo "http://mirror.cabtec.net/alpine/v2.7/main" > /etc/apk/repositories
+echo "http://mirror.cabtec.net/alpine/v3.0/main" > /etc/apk/repositories
 
 apk update
 apk upgrade
