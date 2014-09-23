bdf21d5518ab4be06094f9c4485aed6ef0328b21
Christian Berendt <berendt@b1-systems.de>
2014-09-23 13:20:05 +0200

    Remove Alpine template

--- scripts/alpine/sshd.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh -eux
-
-echo "UseDNS no" >> /etc/ssh/sshd_config
--- scripts/alpine/update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -eux
-
-cp /etc/apk/repositories /etc/apk/repositories.orig
-echo "http://mirror.cabtec.net/alpine/v3.0/main" > /etc/apk/repositories
-
-apk update
-apk upgrade
