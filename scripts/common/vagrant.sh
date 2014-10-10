92dbcfcbd0836a490d91e73c819117991c3119de
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2014-10-10 17:39:52 +0200

    Use the local vagrant public key file

--- scripts/common/vagrant.sh
+++ scripts/common/vagrant.sh
@@ -4,7 +4,7 @@ set -x
 
 mkdir /home/vagrant/.ssh
 wget --no-check-certificate \
-    'https://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' \
+    $URL \
     -O /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
 chmod -R go-rwsx /home/vagrant/.ssh
