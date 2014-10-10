aeb3e474a3c4ebb5a190438ba681ea390f8cb1ac
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2014-10-10 16:24:52 +0200

    Use our local repositories

--- scripts/opensuse/13.1/update.sh
+++ scripts/opensuse/13.1/update.sh
@@ -2,12 +2,10 @@
 
 set -x
 
-rm -f /etc/zypp/repos.d/openSUSE-13.1-1.10.repo
-rpm --import http://download.opensuse.org/distribution/13.1/repo/oss/gpg-pubkey-3dbdc284-4be1884d.asc
-rpm --import http://download.opensuse.org/distribution/13.1/repo/oss/gpg-pubkey-307e3d54-4be01a65.asc
-zypper ar http://download.opensuse.org/distribution/13.1/repo/oss/ opensuse-13.1-oss
-zypper ar http://download.opensuse.org/distribution/13.1/repo/non-oss/ opensuse-13.1-non-oss
-zypper ar http://download.opensuse.org/update/13.1/ opensuse-13.1-update
-zypper ar http://download.opensuse.org/update/13.1-non-oss/ opensuse-13.1-update-non-oss
+zypper rr 1
+zypper ar -n "openSUSE-13.1 OSS" http://linuxmirror.rz.evosec.de/opensuse/distribution/13.1/repo/oss/ repo-13.1-oss
+zypper ar -n "openSUSE-13.1 Non-OSS" http://linuxmirror.rz.evosec.de/opensuse/distribution/13.1/repo/non-oss/ repo-13.1-non-oss
+zypper ar -f -n "openSUSE-13.1 Updates OSS" http://linuxmirror.rz.evosec.de/opensuse/update/13.1/ repo-13.1-update-oss
+zypper ar -f -n "openSUSE-13.1 Updates Non-OSS" http://linuxmirror.rz.evosec.de/opensuse/update/13.1-non-oss/ repo-13.1-update-non-oss
 zypper refresh
 zypper update -y
