84c450ef09124afc3191c6cf6c2099fb17deefba
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2014-10-13 14:06:57 +0200

    Copy vagrant.pub using file provisioner

--- scripts/common/vagrant.sh
+++ scripts/common/vagrant.sh
@@ -3,7 +3,6 @@
 set -x
 
 mkdir /home/vagrant/.ssh
-wget $VAGRANT_KEY_URL \
-    -O /home/vagrant/.ssh/authorized_keys
+mv /tmp/vagrant.pub /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
 chmod -R go-rwsx /home/vagrant/.ssh
