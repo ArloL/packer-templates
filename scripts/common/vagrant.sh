ab26a8b37e808e051ee0d1cf872cebc20c390dfb
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2014-10-13 10:21:36 +0200

    rename environment variable

--- scripts/common/vagrant.sh
+++ scripts/common/vagrant.sh
@@ -3,8 +3,7 @@
 set -x
 
 mkdir /home/vagrant/.ssh
-wget --no-check-certificate \
-    $URL \
+wget $VAGRANT_KEY_URL \
     -O /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
 chmod -R go-rwsx /home/vagrant/.ssh
