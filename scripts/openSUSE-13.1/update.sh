3162ca8ff49b43c87b26e835e33286c2f5a27c7e
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2015-07-24 16:34:05 +0200

    Remove unnecessary options

--- scripts/openSUSE-13.1/update.sh
+++ scripts/openSUSE-13.1/update.sh
@@ -3,9 +3,9 @@
 set -x
 
 zypper removerepo 1
-zypper addrepo --non-interactive "openSUSE-13.1 OSS" http://linuxmirror.rz.evosec.de/opensuse/distribution/13.1/repo/oss/ repo-13.1-oss
-zypper addrepo --non-interactive "openSUSE-13.1 Non-OSS" http://linuxmirror.rz.evosec.de/opensuse/distribution/13.1/repo/non-oss/ repo-13.1-non-oss
-zypper addrepo --force --non-interactive "openSUSE-13.1 Updates OSS" http://linuxmirror.rz.evosec.de/opensuse/update/13.1/ repo-13.1-update-oss
-zypper addrepo --force --non-interactive "openSUSE-13.1 Updates Non-OSS" http://linuxmirror.rz.evosec.de/opensuse/update/13.1-non-oss/ repo-13.1-update-non-oss
+zypper addrepo "openSUSE-13.1 OSS" http://linuxmirror.rz.evosec.de/opensuse/distribution/13.1/repo/oss/ repo-13.1-oss
+zypper addrepo "openSUSE-13.1 Non-OSS" http://linuxmirror.rz.evosec.de/opensuse/distribution/13.1/repo/non-oss/ repo-13.1-non-oss
+zypper --refresh addrepo "openSUSE-13.1 Updates OSS" http://linuxmirror.rz.evosec.de/opensuse/update/13.1/ repo-13.1-update-oss
+zypper --refresh addrepo "openSUSE-13.1 Updates Non-OSS" http://linuxmirror.rz.evosec.de/opensuse/update/13.1-non-oss/ repo-13.1-update-non-oss
 zypper --non-interactive refresh
 zypper --non-interactive update
