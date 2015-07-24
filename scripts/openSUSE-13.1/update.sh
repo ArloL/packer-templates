dc1cce25f3f34b84375b1fb4e34c0f93491fe795
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2015-07-24 17:08:36 +0200

    --non-interactive is a global option before the command

--- scripts/openSUSE-13.1/update.sh
+++ scripts/openSUSE-13.1/update.sh
@@ -2,10 +2,10 @@
 
 set -x
 
-zypper removerepo 1
-zypper addrepo --name "openSUSE-13.1 OSS" http://linuxmirror.rz.evosec.de/opensuse/distribution/13.1/repo/oss/ repo-13.1-oss
-zypper addrepo --name "openSUSE-13.1 Non-OSS" http://linuxmirror.rz.evosec.de/opensuse/distribution/13.1/repo/non-oss/ repo-13.1-non-oss
-zypper addrepo --refresh --name "openSUSE-13.1 Updates OSS" http://linuxmirror.rz.evosec.de/opensuse/update/13.1/ repo-13.1-update-oss
-zypper addrepo --refresh --name "openSUSE-13.1 Updates Non-OSS" http://linuxmirror.rz.evosec.de/opensuse/update/13.1-non-oss/ repo-13.1-update-non-oss
-zypper refresh --non-interactive
-zypper update --non-interactive
+zypper --non-interactive removerepo 1
+zypper --non-interactive addrepo --name "openSUSE-13.1 OSS" http://linuxmirror.rz.evosec.de/opensuse/distribution/13.1/repo/oss/ repo-13.1-oss
+zypper --non-interactive addrepo --name "openSUSE-13.1 Non-OSS" http://linuxmirror.rz.evosec.de/opensuse/distribution/13.1/repo/non-oss/ repo-13.1-non-oss
+zypper --non-interactive addrepo --refresh --name "openSUSE-13.1 Updates OSS" http://linuxmirror.rz.evosec.de/opensuse/update/13.1/ repo-13.1-update-oss
+zypper --non-interactive addrepo --refresh --name "openSUSE-13.1 Updates Non-OSS" http://linuxmirror.rz.evosec.de/opensuse/update/13.1-non-oss/ repo-13.1-update-non-oss
+zypper --non-interactive refresh
+zypper --non-interactive update
