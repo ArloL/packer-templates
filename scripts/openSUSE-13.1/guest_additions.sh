a11eb6b37f9e86367cb8e4d00faa01cf8f20b2f0
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2015-08-10 10:10:37 +0200

    Print uname and available kernels. Do not install make, gcc and kernel-default-devel

--- scripts/openSUSE-13.1/guest_additions.sh
+++ scripts/openSUSE-13.1/guest_additions.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+uname -a
+rpm -qa kernel\*
 zypper --non-interactive remove virtualbox-guest-tools virtualbox-guest-kmp-default
-zypper --non-interactive install make gcc kernel-default-devel
 mkdir -p /mnt/vboxguestadditions
 mount -o loop VBoxGuestAdditions.iso /mnt/vboxguestadditions
 yes | sh /mnt/vboxguestadditions/VBoxLinuxAdditions.run
