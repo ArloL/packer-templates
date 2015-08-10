e0dd9fc66250df80e2f3ff8d5076704f6b0f9d8b
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2015-08-10 07:56:36 +0200

    Remove existing guest additions

--- scripts/openSUSE-13.1/guest_additions.sh
+++ scripts/openSUSE-13.1/guest_additions.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+zypper remove virtualbox-guest-tools virtualbox-guest-kmp-default
 zypper install make gcc kernel-default-devel
 mkdir -p /mnt/vboxguestadditions
 mount -o loop VBoxGuestAdditions.iso /mnt/vboxguestadditions
