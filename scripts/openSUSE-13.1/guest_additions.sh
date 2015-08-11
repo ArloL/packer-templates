e55cfc5c0ba5125c7cc34f70ef2f4943cb246b20
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2015-08-11 09:55:14 +0200

    The iso is in /root not /home/vagrant

--- scripts/openSUSE-13.1/guest_additions.sh
+++ scripts/openSUSE-13.1/guest_additions.sh
@@ -6,8 +6,8 @@ uname -a
 rpm -qa kernel\*
 zypper --non-interactive remove virtualbox-guest-tools virtualbox-guest-kmp-default
 mkdir -p /mnt/vboxguestadditions
-mount -o loop VBoxGuestAdditions.iso /mnt/vboxguestadditions
+mount -o loop /root/VBoxGuestAdditions.iso /mnt/vboxguestadditions
 yes | sh /mnt/vboxguestadditions/VBoxLinuxAdditions.run
 umount /mnt/vboxguestadditions
 rm -rf /mnt/vboxguestadditions
-rm VBoxGuestAdditions.iso
+rm /root/VBoxGuestAdditions.iso
