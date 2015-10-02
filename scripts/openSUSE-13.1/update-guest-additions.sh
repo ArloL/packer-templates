ae537f3273f9ad61d6e909e5dbf22d01821b9449
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2015-10-02 16:23:38 +0200

    Install guest additions without x11

--- scripts/openSUSE-13.1/update-guest-additions.sh
+++ scripts/openSUSE-13.1/update-guest-additions.sh
@@ -9,7 +9,7 @@ uname -a
 rpm -qa kernel\*
 mkdir -p /mnt/vboxguestadditions
 mount -o loop /home/vagrant/VBoxGuestAdditions.iso /mnt/vboxguestadditions
-yes | sh /mnt/vboxguestadditions/VBoxLinuxAdditions.run
+yes | sh /mnt/vboxguestadditions/VBoxLinuxAdditions.run --nox11
 umount /mnt/vboxguestadditions
 rm -rf /mnt/vboxguestadditions
 rm /home/vagrant/VBoxGuestAdditions.iso
