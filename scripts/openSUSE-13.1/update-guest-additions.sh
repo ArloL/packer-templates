e6ae4ca7a1783b26bb973bd80428d857b8a92594
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2015-10-02 16:19:53 +0200

    Correct guest additions path

--- scripts/openSUSE-13.1/update-guest-additions.sh
+++ scripts/openSUSE-13.1/update-guest-additions.sh
@@ -8,8 +8,8 @@ zypper --non-interactive remove virtualbox-guest-kmp-default virtualbox-guest-to
 uname -a
 rpm -qa kernel\*
 mkdir -p /mnt/vboxguestadditions
-mount -o loop /root/VBoxGuestAdditions.iso /mnt/vboxguestadditions
+mount -o loop /home/vagrant/VBoxGuestAdditions.iso /mnt/vboxguestadditions
 yes | sh /mnt/vboxguestadditions/VBoxLinuxAdditions.run
 umount /mnt/vboxguestadditions
 rm -rf /mnt/vboxguestadditions
-rm /root/VBoxGuestAdditions.iso
+rm /home/vagrant/VBoxGuestAdditions.iso
