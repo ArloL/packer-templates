e17d98e37d50d3c2ceb103724caa75b565781c1b
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2017-07-31 16:57:18 +0200

    Add batch files for easier execution of builds

---
+++ openSUSE-42.2-DVD-x86_64.bat
@@ -0,0 +1,3 @@
+packer build openSUSE-42.2-DVD-x86_64.json
+vagrant box add --name evosec-openSUSE-42.2-DVD-x86_64 --force packer_openSUSE-42.2-DVD-x86_64_virtualbox.box
+rm packer_openSUSE-42.2-DVD-x86_64_virtualbox.box
