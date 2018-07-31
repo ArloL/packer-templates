7a5c95f4322e41541446304a69c5af85b4abeb76
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2018-07-31 16:26:37 +0200

    Update vagrant.sh

--- scripts/common/vagrant.sh
+++ scripts/common/vagrant.sh
@@ -3,7 +3,20 @@
 set -e
 set -x
 
-mkdir --parents --mode=700 /home/vagrant/.ssh
+mkdir --parents --mode=0700 /home/vagrant/.ssh
 mv /tmp/vagrant.pub /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
 chmod -R go-rwsx /home/vagrant/.ssh
+
+mkdir --parents --mode=0744 /vagrant
+chown -R vagrant /vagrant
+
+if [ -d /etc/sudoers.d ]
+then
+  echo "vagrant ALL=(ALL) NOPASSWD: ALL" >| /etc/sudoers.d/vagrant
+  chmod 0440 /etc/sudoers.d/vagrant
+else
+  echo "vagrant ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+fi
+
+exit 0
